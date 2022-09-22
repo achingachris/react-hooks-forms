295688228c9516148f3ae8490013113ab7cedb76
chrisachinga <chris.achinga@student.moringaschool.com>
2022-09-23 01:13:25 +0300

    event handlers

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,24 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
 function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+  const [firstName, setFirstName] = useState('John')
+  const [lastName, setLastName] = useState('Henry')
+
+  function handleFirstNameChange(event) {
+    setFirstName(event.target.value)
+  }
+
+  function handleLastNameChange(event) {
+    setLastName(event.target.value)
+  }
 
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
-      <button type="submit">Submit</button>
+      <input type='text' value={firstName} onChange={handleFirstNameChange} />
+      <input type='text' value={lastName} onChange={handleLastNameChange} />
+      <button type='submit'>Submit</button>
     </form>
-  );
+  )
 }
 
-export default Form;
+export default Form
